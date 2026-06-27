087f25f3dd18403473a7e44d39815e04fe6b7e72
dzaeltic <devind224@gmail.com>
2026-06-27 14:27:04 -0500

    Add / base rules to eslint config (AIRBNB)

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const App = () => <h1>Hello, world!</h1>;
+const App = () => {
+  return <h1>Hello, world!</h1>;
+};
 
 export default App;
---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import pkg from 'eslint-config-airbnb';
+// import { defineConfig } from 'eslint/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+export default [
+  ...compat.extends('airbnb', 'airbnb/hooks'),
+  ...compat.env({
+    browser: true,
+    node: true,
+  }),
+  ...compat.plugins('react', 'jsx-a11y'),
+  { 
+    files: ['**/*.jsx', '**/*.js'],
+    rules: {
+      'linebreak-style': 'off',
+      'react/function-component-definition': 'off'
+    }
+  },
+]
