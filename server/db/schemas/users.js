982c3bb956b02edf0a9ba3304fef6a9c41d55d1c
ClaireHappel <clairehap1@gmail.com>
2026-06-29 12:36:05 -0500

    update / users schema

--- server/db/schemas/users.js
+++ server/db/schemas/users.js
@@ -1,7 +1,13 @@
 const { Schema, model } = require('mongoose');
 
+// { id: profile.id ,
+// displayName: profile.displayName,
+// emails: profile.emails[0].value }
+
 const usersSchema = new Schema({
-  name: String,
-});
+  googleId: { type: String, required: true, unique: true },
+  displayName: { type: String, required: true },
+  emails: { type: String, required: true },
+}, { timestamps: true });
 
 module.exports = model('Users', usersSchema);
