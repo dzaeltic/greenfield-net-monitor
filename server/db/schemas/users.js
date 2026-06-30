7200de5dde20c09ffbd61010cab86fa3b74e8e35
ClaireHappel <clairehap1@gmail.com>
2026-06-30 10:01:45 -0500

    update /  usersSchema- emails key changed to email

--- server/db/schemas/users.js
+++ server/db/schemas/users.js
@@ -7,7 +7,7 @@ const { Schema, model } = require('mongoose');
 const usersSchema = new Schema({
   googleId: { type: String, required: true, unique: true },
   displayName: { type: String, required: true },
-  emails: { type: String, required: true },
+  email: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = model('Users', usersSchema);
